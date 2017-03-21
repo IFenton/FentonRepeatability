46805c643af6b3bc5fe2684c2796a2d153d4fae5
Isabel Fenton <isabf@nhm.ac.uk>
2017-03-21 17:57:32 +0000

    Working on confidence

--- Repeatability.R
+++ Repeatability.R
@@ -40,6 +40,7 @@ str(completeIDs)
 # list of people in the analysis
 people <- gsub("ID", "", grep("ID", names(completeIDs), value = TRUE))
 people.col <- grep("ID", names(completeIDs), value = TRUE)
+p.conf.col <- grep("C$", names(completeIDs), value = TRUE)
 
 people.df <- data.frame(Name = people)
 str(people.df)
@@ -53,22 +54,25 @@ species.df <- data.frame(Species = checklist$Species)
 
 # 3. Person level statistics -----------------------------
 
+# 3a. All individuals -----------------------------------------------------
 # Number of species identified
-people.df$NumSpecID <- apply(completeIDs[, people.col], 2, function(x) sum(unique(x) %in% species))
+people.df$NumSpID <- apply(completeIDs[, people.col], 2, function(x) sum(unique(x) %in% species))
 
 # Number of genera identified
 unique(grep("^[[:upper:]]", unlist(strsplit(as.character(completeIDs$DefinitiveID), " ")), value = TRUE))
 people.df$NumGenID <- apply(completeIDs[, people.col], 2, function(x) length(unique(grep("^[[:upper:]]", unlist(strsplit(as.character(x), " ")), value = TRUE))))
 
 # Number of specimens lost
-sum(completeIDs$DefinitiveID == "lost")
+sum(completeIDs$PooleID != "lost")
+sum(completeIDs$PooleID != "lost"& completeIDs$PooleID %in% checklist$Species)
 people.df$numLost <- apply(completeIDs[, people.col], 2, function(x) sum(x == "lost"))
-people.df$TotalInd <- apply(completeIDs[, people.col], 2, function(x) sum(x != "lost"))
+people.df$TotalIndSp <- apply(completeIDs[, people.col], 2, function(x) sum(x != "lost" & x %in% checklist$Species))
+people.df$TotalIndGe <- apply(completeIDs[, people.col], 2, function(x) sum(x != "lost"))
 
 # Fraction of species identified
 people.df$numUnIDd <- apply(completeIDs[, people.col], 2, function(x) sum(x == "unIDd"))
 
-people.df$fracSpecIDd <- people.df$NumSpecID / people.df$NumSpecID[people.df$Name == "Definitive"]
+people.df$fracSpecIDd <- people.df$NumSpID / people.df$NumSpID[people.df$Name == "Definitive"]
 
 # Fraction of genera identified
 people.df$fracGenIDd <- people.df$NumGenID / people.df$NumGenID[people.df$Name == "Definitive"]
@@ -92,6 +96,25 @@ sum(gsub(" .*", "", completeIDs$FentonID) == gsub(" .*", "", completeIDs$Definit
 
 people.df$fracCorrGen <- apply(completeIDs[, people.col], 2, function(x) sum(gsub(" .*", "", x) == gsub(" .*", "", completeIDs$DefinitiveID)) / sum(x != "lost"))
 mean(people.df$fracCorrGen)
+
+# 3b. Split by confidence -------------------------------------------------
+# number of identifications by confidence (working at species level)
+sum(completeIDs$FentonC == "y", na.rm = TRUE)
+
+people.df$numCy <- NA
+people.df$numCy[3:nrow(people.df)] <- apply(completeIDs[, p.conf.col], 2, function(x) sum(x == "y", na.rm = TRUE))
+
+people.df$numCm <- NA
+people.df$numCm[3:nrow(people.df)] <- apply(completeIDs[, p.conf.col], 2, function(x) sum(x == "m", na.rm = TRUE))
+
+people.df$numCn <- NA
+people.df$numCn[3:nrow(people.df)] <- apply(completeIDs[, p.conf.col], 2, function(x) sum(x == "n", na.rm = TRUE))
+
+people.df
+
+# fraction of confident identifications
+
+
 
 # 4. Percentage accuracy per specimen ------------------------------------
 
